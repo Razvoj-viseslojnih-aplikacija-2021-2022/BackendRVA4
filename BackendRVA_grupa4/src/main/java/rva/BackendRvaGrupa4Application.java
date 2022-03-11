50484eeb3a284590de795409a5799410e211cf3c
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-03-11 16:50:08 +0100

    Početni projekat

---
+++ BackendRVA_grupa4/src/main/java/rva/BackendRvaGrupa4Application.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRvaGrupa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRvaGrupa4Application.class, args);
+	}
+
+}
