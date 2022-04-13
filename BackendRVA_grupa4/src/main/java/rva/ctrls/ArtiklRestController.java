9fc81d835d7599e7be6deb186bc9355fac51d518
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-04-13 10:30:05 +0200

    Podešavanja Swagger-a

--- BackendRVA_grupa4/src/main/java/rva/ctrls/ArtiklRestController.java
+++ BackendRVA_grupa4/src/main/java/rva/ctrls/ArtiklRestController.java
@@ -14,10 +14,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import rva.jpa.Artikl;
 import rva.repositories.ArtiklRepository;
 
 @RestController
+@Api(tags = {"Artikl CRUD operacije"})
 public class ArtiklRestController {
 
 	@Autowired
@@ -27,19 +30,23 @@ public class ArtiklRestController {
 	private JdbcTemplate jdbcTemplate;
 	
 	@GetMapping("artikl")
+	@ApiOperation(value = "Vraca kolekciju svih artikala iz baze podataka")
 	public Collection<Artikl> getArtikli() {
 		return artiklRepository.findAll();
 	}
 	
 	@GetMapping("artikl/{id}")
+	@ApiOperation(value = "Vraca artikl u odnosu na posledjenu vrednost path varijable id")
 	public Artikl getArtikl(@PathVariable("id") Integer id) {
 		return artiklRepository.getOne(id);
 	}
 	@GetMapping("artiklNaziv/{naziv}")
+	@ApiOperation(value = "Vraca artikl u odnosu na posledjenu vrednost path varijable id")
 	public Collection<Artikl> getArtikliByNaziv(@PathVariable("naziv") String naziv) {
 		return artiklRepository.findByNazivContainingIgnoreCase(naziv);
 	}
 	@PostMapping("artikl")
+	@ApiOperation(value = "Dodaje novi artikl u bazu podataka.")
 	public ResponseEntity<Artikl> insertArtikl(@RequestBody Artikl artikl) {
 		if (!artiklRepository.existsById(artikl.getId())) {
 			artiklRepository.save(artikl);
@@ -49,6 +56,7 @@ public class ArtiklRestController {
 	}
 	
 	@PutMapping("artikl")
+	@ApiOperation(value = "Update-uje postojeći artikl.")
 	public ResponseEntity<Artikl> updateArtikl(@RequestBody Artikl artikl) {
 		if (artiklRepository.existsById(artikl.getId())) {
 			artiklRepository.save(artikl);
@@ -57,6 +65,7 @@ public class ArtiklRestController {
 		return new ResponseEntity<Artikl>(HttpStatus.NO_CONTENT);
 	}
 	@DeleteMapping("artikl/{id}")
+	@ApiOperation(value = "Briše artikl u odnosu na vrednost posleđene path varijable id.")
 	public ResponseEntity<Artikl> deleteArtikl(@PathVariable("id") Integer id) {
 		if (artiklRepository.existsById(id)) {
 			artiklRepository.deleteById(id);
